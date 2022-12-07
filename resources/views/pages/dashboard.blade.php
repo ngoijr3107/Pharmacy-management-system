d666ea5eb9fda2db9d16e2b128f3f75933c808e2
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2022-12-07 18:07:36 +0300

    Dashboard changed to pharmacy related

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -8,45 +8,39 @@
         <div class="col-md-3">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="text-center">Total Candidates</h5>
-                    <h5 class="text-right">15</h5>
-                </div>
-                <div class="card-footer text-center">
-                    <a href="#">See more<i class="fas fa-arrow-right"></i></a>
+                    <h5 class="">Stock count</h5>
+                    <small class="text-right">15</small>
                 </div>
             </div>
         </div>
         <div class="col-md-3">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="text-center">Total Voters</h5>
-                    <h5 class="text-right">325</h5>
+                    <h5 class="">Orders</h5>
+                    <small class="text-right">325</small>
                 </div>
-                <div class="card-footer text-center">
+                <!-- <div class="card-footer text-center">
                     <a href="#">See more<i class="fas fa-arrow-right"></i></a>
+                </div> -->
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="">Sales</h5>
+                    <small class="text-right">3</small>
                 </div>
             </div>
         </div>
         <div class="col-md-3">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="text-center">Total Cartegories</h5>
-                    <h5 class="text-right">3</h5>
+                    <h5 class="">Expenses</h5>
+                    <small class="text-right">14000</small>
                 </div>
-                <div class="card-footer text-center">
+                <!-- <div class="card-footer text-center">
                     <a href="#">See more<i class="fas fa-arrow-right"></i></a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="text-center">Total Votes</h5>
-                    <h5 class="text-right">14000</h5>
-                </div>
-                <div class="card-footer text-center">
-                    <a href="#">See more<i class="fas fa-arrow-right"></i></a>
-                </div>
+                </div> -->
             </div>
         </div>
      </div>
