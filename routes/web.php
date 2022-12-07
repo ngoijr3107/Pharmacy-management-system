feaf0969dc369abf76ec91dfc21c46b24a0bd94d
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2022-12-07 18:08:22 +0300

    routes for category and medicine

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,10 +15,18 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
  */
-
+// Routes defined
 Route::get('/', function () {
     return view('pages.dashboard');
 });
+Route::get('/category', function(){
+    return view('category.index');
+});
+Route::get('/medicine', function(){
+    return view('medicine.index');
+});
+
+Route::get('/category', [CategoryController::class, 'index']);
 
 Route::get('/user', [UserController::class, 'index']);
 
