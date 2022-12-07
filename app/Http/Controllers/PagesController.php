972ff53eb32be06f855472808072c5a98ec2a00e
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2022-12-07 18:06:13 +0300

    category and medicine routes

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,4 +9,12 @@ class PagesController extends Controller
     public function dashboard(){
         return view('pages.dashboard');
     }
+
+    public function category(){
+        return view('category.index');
+    }
+
+    public function medicine(){
+        return view('medicine.index');
+    }
 }
