461d9f86ab1b7ed12c16bdc83347373c61c347fe
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2022-12-13 16:37:12 +0300

    favicon added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <title>Pharmacy System</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset("assets/images/favicon.png")}}">
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
 		
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
