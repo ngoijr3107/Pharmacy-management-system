79cb54ab62e67bdce03057d565f44dded4a216a3
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2022-12-13 19:18:37 +0300

    title

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <title>Pharmacy System</title>
+    <title>Digtal Pharmacy</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset("assets/images/favicon.png")}}">
+    <link rel="icon" href="{{ asset('assets/images/favicon.png') }}">
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
-		
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-		<link rel="stylesheet" href="{{asset("assets/css/style.css")}}">
-</head>
-<body>  
-   
-       
 
-      <script src="{{asset("assets/js/jquery.min.js")}}"></script>
-      <script src="{{asset("assets/js/popper.js")}}"></script>
-      <script src="{{asset("assets/js/bootstrap.min.js")}}"></script>
-      <script src="{{asset("assets/js/main.js")}}"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+</head>
+
+<body>
+
+
+
+    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/js/popper.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
 </body>
+
 </html>
 @yield('content')
