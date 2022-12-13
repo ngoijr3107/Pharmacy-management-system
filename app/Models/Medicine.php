a2e98c4062a98b2cb6e4d77033ea8a76deee1b26
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2022-12-13 16:35:02 +0300

    relationship to category

--- app/Models/Medicine.php
+++ app/Models/Medicine.php
@@ -4,12 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
 
 class Medicine extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'name', 'category', 'store_box', 'purchase_price', 'sell_price', 'qty',
+        'name', 'category_id', 'store_box', 'purchase_price', 'sell_price', 'qty',
         'generic_name', 'company', 'effects', 'expiry_date'
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class,'category_id','id');
+
+    }
 }
