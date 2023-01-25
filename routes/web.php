450afba149a53790295ccd640de476588b80a890
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2023-01-25 17:37:38 +0300

    add auth to routes

--- routes/web.php
+++ routes/web.php
@@ -17,33 +17,38 @@ use Illuminate\Support\Facades\Route;
 |
  */
 // Routes defined
-Route::get('/', function () {
+Auth::routes([
+    'register' => false,
+]);
+Route::get('/', function(){
     return view('auth.login');
 });
-Route::get('/category', function(){
-    return view('category.index');
+
+Route::group(['middleware' => 'auth'], function () {
+
+            Route::get('/category', function(){
+                return view('category.index');
+            });
+            Route::get('/medicine', function(){
+                return view('medicine.index');
+            });
+
+            // Category Routes
+            Route::get('/category', [CategoryController::class, 'index']);
+            Route::post('category/create', [CategoryController::class, 'store']);
+            Route::get('category/edit/{id}', [CategoryController::class, 'edit']);
+            Route::patch('category/edit', [CategoryController::class, 'update']);
+            Route::delete('category/{id}', [CategoryController::class, 'destroy']);
+
+            Route::get('/medicine', [MedicineController::class, 'index']);
+            Route::post('medicine/create',[MedicineController::class, 'store']);
+            Route::put('medicine',[MedicineController::class, 'update']);
+
+            Route::get('/user', [UserController::class, 'index']);
+
+            // Route::get('/dashboard', [PagesController::class, 'dashboard']);
+            
+
+            Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 });
-Route::get('/medicine', function(){
-    return view('medicine.index');
-});
-
-Route::get('/category', [CategoryController::class, 'index']);
-Route::post('category/create', [CategoryController::class, 'store']);
-Route::get('category/edit/{id}', [CategoryController::class, 'edit']);
-Route::patch('category/edit', [CategoryController::class, 'update']);
-Route::delete('category/{id}', [CategoryController::class, 'destroy']);
-
-Route::get('/medicine', [MedicineController::class, 'index']);
-Route::post('medicine/create',[MedicineController::class, 'store']);
-Route::put('medicine',[MedicineController::class, 'update']);
-
-Route::get('/user', [UserController::class, 'index']);
-
-// Route::get('/dashboard', [PagesController::class, 'dashboard']);
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
