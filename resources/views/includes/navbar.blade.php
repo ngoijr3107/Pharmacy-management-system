70c8234aa4e4f152ecfbfe87c6e9bc33f8b04a69
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2023-01-25 18:51:35 +0300

    profile route

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -29,6 +29,11 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ url('/profile') }}"
+                            >
+                                {{ __('Profile') }}
+                            </a>
+                            <hr>
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
                                    document.getElementById('logout-form').submit();">
