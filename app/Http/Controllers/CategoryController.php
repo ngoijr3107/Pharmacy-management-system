2801cef34839e4921e6bf763b224c575d82fe452
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2023-01-25 14:57:43 +0300

    Category get latest

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,7 +10,7 @@ class CategoryController extends Controller
 
     public function index()
     {
-        $category = Category::all();
+        $category = Category::latest()->get();
         return view('category.index')->with('categories',$category);
     }
 
