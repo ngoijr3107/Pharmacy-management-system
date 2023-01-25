4ae70f20f8221b6a237b202afc15d686c61df16c
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2023-01-25 18:08:47 +0300

    Add stock count summary

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Medicine;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,10 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('pages.dashboard');
+        $stock_count = Medicine::get()->count();
+
+        return view('pages.dashboard', [
+            'stock_count' => $stock_count,
+        ]);
     }
 }
