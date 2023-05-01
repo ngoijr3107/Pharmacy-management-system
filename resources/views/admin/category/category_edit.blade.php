1ae679d53ddd91928bf77d504c89bf16bbfb1771
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2023-05-01 19:21:54 +0300

    edit category blade

---
+++ resources/views/admin/category/category_edit.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.admin')
+@section('content')
+    <div class="container px-6 mx-auto">
+        <div class="px-4 py-3 mb-8 mt-8 bg-white rounded-lg shadow-md dark:bg-gray-800 flex flex-col my-2">
+            <h2 class="mb-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
+                Edit category
+            </h2>
+            <form action="{{ route('admin.CategoryUpdate', ['category' => $category->id]) }}" method="POST">
+                @csrf
+                @method('put')
+                <div class="-mx-3 md:flex mb-6">
+                    <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label class="block text-sm">
+                            <span class="text-gray-700 dark:text-gray-400">Name</span>
+                            <input
+                                class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                                name="category_name" value="{{ $category->category_name }}" placeholder="enter category name..." />
+                        </label>
+                    </div>
+
+                    <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label class="block text-sm">
+                            <span class="text-gray-700 dark:text-gray-400">Description</span>
+                            <input
+                                class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                                name="description" value="{{ $category->description }}" placeholder="enter category description..." />
+                        </label>
+                    </div>
+                </div>
+
+                <div class="-mx-3 md:flex mb-6">
+                    <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+                        <button type="submit"
+                            class="btn btn-primary px-3 mb-2 py-1 text-md font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
+                            Save
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
