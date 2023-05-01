b0a51430f57f6dea309e75bd4189d083a9f19595
Paschal Ngoi <47353288+ngoijr3107@users.noreply.github.com>
2023-05-01 19:22:54 +0300

    add font awesome

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>Login - Windmill Dashboard</title>
+    <title>{{ config('app.name' )}}</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet"/>
     <link rel="stylesheet" href="{{ asset('assets/css/tailwind.output.css') }}"/>
 
+    <script src="https://kit.fontawesome.com/e5f26e6179.js" crossorigin="anonymous"></script>
 </head>
 <body>
 <div class="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
