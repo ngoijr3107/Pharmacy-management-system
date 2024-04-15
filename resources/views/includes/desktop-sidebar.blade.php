be4c4df657c2e33087aa3d2af1665df5ccc5ff42
Paschal Mizengo <47353288+ngoijr3107@users.noreply.github.com>
2024-04-15 12:15:39 +0300

    add supliers menu

--- resources/views/includes/desktop-sidebar.blade.php
+++ resources/views/includes/desktop-sidebar.blade.php
@@ -58,6 +58,23 @@
                 </a>
             </li>
 
+            <li class="relative px-6 py-3">
+                @if (request()->routeIs('admin.supplier'))
+                    <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                        aria-hidden="true"></span>
+                @endif
+                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                    href="{{ route('admin.supplier') }}">
+                    <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
+                        stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                        <path
+                            d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z">
+                        </path>
+                    </svg>
+                    <span class="ml-4">Suppliers</span>
+                </a>
+            </li>
+
             {{-- <li class="relative px-6 py-3">
                 <button
                     class="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
