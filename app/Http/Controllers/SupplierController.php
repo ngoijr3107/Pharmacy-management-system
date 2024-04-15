9ff7e607689d3dbedb897e2621208dae90555a6a
Paschal Mizengo <47353288+ngoijr3107@users.noreply.github.com>
2024-04-15 11:36:38 +0300

    supplier controller add

---
+++ app/Http/Controllers/SupplierController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Supplier;
+
+class SupplierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $suppliers = Supplier::latest()->paginate(10);
+        return view('admin.supplier.supplier')->with('suppliers',$suppliers);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.supplier.supplier_create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'phone' => 'required',
+            'email' => 'required',
+            'city' => 'required',
+            'country' => 'required',
+            'address' => 'required',
+        ]);
+        $supplier = Supplier::create($request->all());
+        $supplier->save();
+        return redirect('/dashboard/supplier')->with('success', 'Supplier has been added');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $supplier = Supplier::findOrFail($id);
+        return view('admin.supplier.supplier_edit', compact('supplier'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'phone' => 'required',
+            'email' => 'required',
+            'city' => 'required',
+            'country' => 'required',
+            'address' => 'required',
+        ]);
+
+        $supplier = Supplier::findOrFail($id);
+        $supplier->fill($request->all());
+        $supplier->save();
+        return redirect('/dashboard/supplier')->with('success', 'Supplier has been updated successfully!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $supplier = Supplier::findOrFail($id);
+        $supplier->delete();
+
+        return redirect('/dashboard/supplier')->with('success', 'Supplier has been deleted successfully!');
+    }
+}
