f8b7adfa1099a1f88dc417ee7ffdf8ea7525a4ba
Paschal Mizengo <47353288+ngoijr3107@users.noreply.github.com>
2024-05-05 03:21:43 +0300

    remove unused components

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container px-6 mx-auto grid">
         <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-            Dashboard
+            Hello {{ Auth()->user()->name }}, Welcome Back!
         </h2>
         <!-- CTA -->
 
